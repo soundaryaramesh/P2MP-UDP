816184b7cd7c4e907f121834116bf01daa3c68f3
soundaryaramesh <soundaryaramesh96@gmail.com>
2016-03-25 01:11:55 +0530

    Modified client and server codes

--- Client.py
+++ Client.py
@@ -14,6 +14,8 @@ def no_of_digits(a):
 
 
 def get_seq_no(a):
+	if(a==0):
+		return '0'*10
 	count=no_of_digits(a)
 	no_of_zeroes=10-count
 	val='0'*no_of_zeroes + '%d' %a
@@ -22,8 +24,11 @@ def get_seq_no(a):
 # Buffer size of client should be smaller than the Server's. Otherwise data loss happens
 s=socket(AF_INET,SOCK_DGRAM)
 no_of_arg=len(sys.argv)
-if(no_of_arg!=3):
-	print "expected format python %s <number of hosts> <filename>"%(sys.argv[0])
+if(no_of_arg!=4):
+	print "expected format python %s <number of hosts> <filename> <MSS> "%(sys.argv[0])
+elif int(sys.argv[3])<89 or int(sys.argv[3])>2048:
+	print sys.agrv[2]," should be greater than 89 bytes and lesser than 2048 bytes"
+
 else:
 	host=[]
 	addr=[]
@@ -38,7 +43,8 @@ else:
 	
 		
 	#addr=(host,port)
-	buff_size=1024-47
+	t0=time.time()
+	buff_size=int(sys.argv[3])-47-42
 	
 	file_name=sys.argv[2]
 	file_size=os.stat(file_name).st_size
@@ -52,48 +58,57 @@ else:
 	while(len(a)!=0):
 		d,ad=s.recvfrom(buff_size)
 		a.remove(ad)
-	t2=time.time()
+
 
 	seq=0
 	curr_seq=get_seq_no(seq)
 	f=open(file_name,'rb')
 	data=f.read(buff_size)
-	print data
+	#print data
 	s.settimeout(0.5)
 	
 	while(data):
 		
 		a=addr[:]
 		data=curr_seq+data
+		ascii_val=map(ord,data)
+		group_val=[]
+		for i in range(1,len(ascii_val)):
+			group_val.append(ascii_val[i-1]*256+ascii_val[i])
+		checksum=reduce(lambda x,y:(x+y)%65536,group_val)
+		checksum=checksum^65535
+		chksum_string='0'*5+'%d' %checksum
+		chksum=chksum_string[-5:]
+		#print chksum
+		data=data+chksum
 		host_resp=0
 		while(len(a) is not 0):
 			#print "curr seq no is ",curr_seq
 			for i in range(len(a)):
 				while(s.sendto(data,a[i])):
 					break
-			t0=time.time()
-			t=0
 			
 			while(host_resp<no_of_hosts):
 				#print "waiting for ack.."
 				try:
 					ack,ad=s.recvfrom(buff_size)
-					print "ack from ",ad[0]," for seq no = ",curr_seq
+					#print "ack  = ",ack
 					#if(int(curr_seq) is int(ack)):
 					host_resp=host_resp+1
 				       	a.remove(ad)
 				except:
-					print "timeout seq no = ",curr_seq
+					print "timeout, Sequence number = ",curr_seq
 					break
-			t=time.time()-t0
-			print t
+			
+			#print t
 
 
 			
 		data=f.read(buff_size)
 		seq=seq+1
 		curr_seq=get_seq_no(seq)
-		
+	total_time=time.time()-t0
+	print "Total time is %d"%total_time		
 	s.close()
 	f.close()
 
--- Server.py
+++ Server.py
@@ -2,16 +2,19 @@ from socket import *
 import sys
 import random
 
-host="10.42.0.42"
-port=19000
+
 no_of_arg=len(sys.argv)
+
+
+if(no_of_arg!=4):
+        print "expected format python %s <port no> <filename> <probability>" %(sys.argv[0])
+
+host="10.42.0.42"
+port=int(sys.argv[1])
 randomList=[]
 
-if(no_of_arg!=3):
-        print "expected format python %s <filename> <probability>" %(sys.argv[0])
-
 s=socket(AF_INET,SOCK_DGRAM)
-prob = float(sys.argv[2]);
+prob = float(sys.argv[3]);
 addr=(host,port)
 s.bind(addr)
 buff_size=2048
@@ -20,9 +23,9 @@ msg = 'sequence no. recieved!'
 data,address=s.recvfrom(buff_size)
 s.sendto(msg,address)
 
-file_name=sys.argv[1]
+file_name=sys.argv[2]
 f=open(file_name,'wb')
-print "The total sequences are :  "
+#print "The total sequences are :  "
 print int(data)
 total = int(data)
 
@@ -30,35 +33,59 @@ total = int(data)
 prob = int(prob*total)
 for i in range(prob):
 	gen_no=random.randint(0,total-1)	
-	while(gen_no in randomList):
-		gen_no=random.randint(0,total-1)		
+	"""while(gen_no in randomList):
+		gen_no=random.randint(0,total-1)		"""
 	randomList.append(gen_no)		
-print len(randomList)
+#print len(randomList)
 randomList.sort()
 print randomList
 
 
 try:
 	data,address=s.recvfrom(buff_size)
-	print data[11:]
+	
 	count =0	
-	while(data):
-		ack = data[0:10]
-		#print ack
-		if(count not in randomList):		
-			s.sendto(ack,address)
+	while(True):
+		
+		if(count not in randomList):
 			#print "Sequence Number", ack			
-			if(count==0):			
-				f.write(data[11:])
+			ack=data[0:10]
+			ack_no=int(ack)
+			#print "seq no is %d"%ack_no
+			#print "count is %d"%count
+			if(ack_no<count):
+				while(not s.sendto(ack,address)):
+		       			continue
+				count = count-1
+				print "	ack < count"
+			elif(ack_no==count):
+				ascii_val=map(ord,data[:-5])
+				group_val=[]
+				for i in range(1,len(ascii_val)):
+					group_val.append(ascii_val[i-1]*256+ascii_val[i])
+				
+				checksum=reduce(lambda x,y:(x+y)%65536,group_val)
+				chk = data[-5:]
+				checksum_calculate=(checksum+int(chk))%65535
+				#print checksum_calculate
+				if(checksum_calculate==0):
+		       			while(not s.sendto(ack,address)):
+		       				continue
+					#print ack, "sent"
+		       			f.write(data[10:-5])
+				else:
+					print "checksum didn't match!"
+					count =count-1
 			else:
-				f.write(data[10:])
+				print "Sequence no didn't match"
+				count = count-1
 			
 		else:
-			print "Packet Lost"			
+			print "Packet Loss, Sequence number = %d "%count			
 			randomList.remove(count)
 			count =count-1
 		if(count == total-1):
-			print count is total-1
+			#print count is total-1
 			raise ValueError()
 		data,address=s.recvfrom(buff_size)
 		
